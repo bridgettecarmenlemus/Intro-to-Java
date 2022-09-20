c9c1c52b8e92a49cff058cf3ede606f1d488cbae
Bridgette Lemus <bridgette.lemus.se@gmail.com>
2022-09-20 17:19:52 -0400

    Intro to Java

---
+++ intro-to-java.java
@@ -0,0 +1,8 @@
+// Introduction to Java
+// One reason people love Java is the Java Virtual Machine. This ensures that the same Java code can be run on different operating systems and platforms. The slogan nis "write once, run everywhere"
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.print("Hello World!");
+  }
+}
